a0278bc615cf7d05a7cff819e50ade773212be54
BilgeKaanpc <kashikoi.dev@gmail.com>
2022-07-22 09:02:25 +0300

    Add

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+    [SerializeField]
+    private GameObject[] characters;
+
+    private int _chatIndex;
+    public int CharIndex
+    {
+        get { return _chatIndex; }
+        set { _chatIndex = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+           
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "GamePlay")
+        {
+            Debug.Log(CharIndex);
+            Instantiate(characters[CharIndex]);
+        }
+    }
+}
